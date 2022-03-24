3479f55fd329eecc5e2598e23e7679ca7ba59c6d
Jet Lee <jettdlee@gmail.com>
2022-03-24 12:25:05 +0000

    stream

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,6 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_to "quotes", partials: 'quotes/quote', locals: { quote: self }, target: "quotes" }
 end
