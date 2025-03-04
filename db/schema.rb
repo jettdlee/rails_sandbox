cb28e60f02fbf76539c2e712259c94ba5137df9a
Jet Lee <jettdlee@gmail.com>
2025-03-04 19:24:07 +0000

    add quotes

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_03_202827) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_03_211041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "quotes", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
