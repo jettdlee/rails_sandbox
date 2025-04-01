2cf3833c73373f0699bf15abab8c65f51baad5ea
Jet Lee <jettdlee@gmail.com>
2025-04-01 20:16:56 +0100

    Add turbo stream on items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_later_to "items" }
+  # after_update_commit -> { broadcast_replace_later_to "items" }
+  # after_destroy_commit -> { broadcast_remove_to "items" }
+  broadcasts_to ->(item) { "items" }, inserts_by: :prepend
+
 end
